ed0e96167ae14974d7085e1b283e70732df18db6
GUSTAVO GALVAO <93690941+gugamorais@users.noreply.github.com>
2022-11-08 06:47:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//document.getElementById("btn-laranja"); addEventListener("click",
+//function(){document.querySelector("body").setAttribute("class","laranja");
+//});
+
+document.getElementById("btn-laranja"). addEventListener("click",
+function(){document.querySelector("body"). setAttribute("class","laranja");
+})
+
+document.getElementById("btn-vermelho"). addEventListener("click",
+function(){ document.querySelector("body"). setAttribute("class", "vermelho");
+})
+
+document.getElementById("btn-roxo"). addEventListener("click", 
+function(){document.querySelector("body"). setAttribute("class", "roxo");
+})
